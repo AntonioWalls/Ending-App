b9cece1c27b98d6061642b66b2de80005f719b92
AntonioWalls <psfa.fabian@gmail.com>
2024-04-22 08:36:27 -0700

    asdf

--- src/Grid.js
+++ src/Grid.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css"; 
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import './usuarios.json';
 
 const Grid = () => {
   const [gridApi, setGridApi] = useState(null);
