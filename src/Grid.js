a10a778268f9933cb1a720b1689596826d35f116
AntonioWalls <psfa.fabian@gmail.com>
2024-05-02 23:33:24 -0700

    funciones CRUD con react sin implementar

--- src/Grid.js
+++ src/Grid.js
@@ -3,10 +3,157 @@ import { AgGridReact } from 'ag-grid-react';
 import axios from 'axios';
 import "ag-grid-community/styles/ag-grid.css"; 
 import "ag-grid-community/styles/ag-theme-quartz.css";
-
-
+import { Button } from 'react-bootstrap';
 
 const Grid = () => {
+  const [formData, setFormData] = useState({
+    idUsuario: "",
+    idRol: "",
+    nombre: "",
+    primerApellido: "",
+    segundoApellido: "",
+    fechaNacimiento: "",
+    genero: "",
+    correo: "",
+    telefono: "",
+    nombreUsuario: "",
+    contraseña: "",
+    habilitado: "",
+    nombreCompleto: "",
+    strFechaNacimiento: "",
+    });
+
+    const [editID, setEditID] = useState()
+
+    const [data, setData] = useState([]);
+    const [refresh, setRefresh] = useState(0)
+
+    const { idUsuario,
+            idRol,
+            nombre,
+            primerApellido,
+            segundoApellido, 
+            fechaNacimiento, 
+            genero, 
+            correo, 
+            telefono,
+            nombreUsuario,
+            contraseña,
+            habilitado,
+            nombreCompleto,
+            strFechaNacimiento } = formData;
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (idUsuario &&
+            idRol &&
+            nombre && 
+            primerApellido &&
+            segundoApellido &&
+            fechaNacimiento &&
+            genero &&
+            correo&& 
+            telefono &&
+            nombreUsuario &&
+            contraseña &&
+            habilitado &&
+            nombreCompleto &&
+            strFechaNacimiento) {
+            axios.post('http://187.189.158.186:7777/Usuario', formData)
+                .then(res => {
+                    setData([...data, res.data]);
+                    setFormData({ idUsuario: "", 
+                                  idRol: "",
+                                  nombre: "", 
+                                  primerApellido: "",
+                                  segundoApellido: "",
+                                  fechaNacimiento: "",
+                                  genero: "",
+                                  correo: "",
+                                  telefono: "",
+                                  nombreUsuario: "",
+                                  contraseña: "",
+                                  habilitado: "",
+                                  nombreCompleto: "",
+                                  strFechaNacimiento: "", });
+
+                })
+                .catch(err => console.log(err))
+
+        }
+    };
+
+    const handleUpdate = () => {
+      if (idUsuario &&
+        idRol &&
+        nombre && 
+        primerApellido &&
+        segundoApellido &&
+        fechaNacimiento &&
+        genero &&
+        correo&& 
+        telefono &&
+        nombreUsuario &&
+        contraseña &&
+        habilitado &&
+        nombreCompleto &&
+        strFechaNacimiento) {
+          axios.put(`http://187.189.158.186:7777/Usuario${editID}`, formData)
+              .then(res => {
+                  setFormData({ idUsuario: "", 
+                  idRol: "",
+                  nombre: "", 
+                  primerApellido: "",
+                  segundoApellido: "",
+                  fechaNacimiento: "",
+                  genero: "",
+                  correo: "",
+                  telefono: "",
+                  nombreUsuario: "",
+                  contraseña: "",
+                  habilitado: "",
+                  nombreCompleto: "",
+                  strFechaNacimiento: "",});
+                  setRefresh(refresh + 1)
+              })
+              .catch(err => console.log(err))
+
+      }
+  };
+
+  const handleDelete = (deleteID) => {
+      axios.delete(`http://187.189.158.186:7777/Usuario${deleteID}`)
+      .then(res => {
+        console.log('DELETD RECORD::::', res)
+
+      })
+      .catch(err => console.log(err))
+  };
+
+  const handleEdit = (editIDNotState) => {
+      axios.get(`http://187.189.158.186:7777/Usuario${editIDNotState}`)
+          .then(res => {
+              setFormData(res.data)
+
+          })
+          .catch(err => console.log(err))
+  };
+
+  useEffect(() => {
+      axios.get('http://187.189.158.186:7777/Usuario')
+          .then(res => {
+              setData(res.data)
+          })
+          .catch(err => console.log(err))
+      // console.log(data);
+  }, [refresh]);
+
+
+  //---------------------------------------------------
   const [rowData, setRowData] = useState([]);
 
   useEffect(() => {
@@ -20,18 +167,18 @@ const Grid = () => {
   }, []);
 
   const columnDefs = [
-    { headerName: "idUsuario", field: "idUsuario" },
-    { headerName: "nombre", field: "nombre" },
-    { headerName: "primerApellido", field: "primerApellido" },
-    { headerName: "segundoApellido", field: "segundoApellido" },
-    { headerName: "fechaNacimiento", field: "fechaNacimiento"},
-    { headerName:"nombreUsuario", field: "nombreUsuario"},
-    { headerName:"contraseña", field: "contraseña"},
-    { headerName:"¿habilitado?", field: "habilitado"},
-    { headerName:"nombreCompleto", field: "nombreCompleto"},
-    { headerName:"strFechaNacimiento", field:"strFechaNacimiento"}
-
+    { headerName: "idUsuario", field: "idUsuario", editable:true},
+    { headerName: "nombre", field: "nombre", editable:true },
+    { headerName: "primerApellido", field: "primerApellido", editable:true },
+    { headerName: "segundoApellido", field: "segundoApellido", editable:true },
+    { headerName: "fechaNacimiento", field: "fechaNacimiento", editable:true},
+    { headerName:"nombreUsuario", field: "nombreUsuario", editable:true},
+    { headerName:"contraseña", field: "contraseña", editable:true},
+    { headerName:"¿habilitado?", field: "habilitado", editable:true},
+    { headerName:"nombreCompleto", field: "nombreCompleto", editable:true},
+    { headerName:"strFechaNacimiento", field:"strFechaNacimiento", editable:true}
   ];
+
 
   return (
         <div className="ag-theme-quartz" style={{ height: 600, width:1500 }}>
@@ -39,9 +186,15 @@ const Grid = () => {
         rowData={rowData}
         columnDefs={columnDefs}
         rowSelection={"multiple"}
-          rowMultiSelectWithClick={true}
+        rowMultiSelectWithClick={true}
+        singleClickEdit
+        stopEditingWhenCellsLoseFocus
+
       />
+      
+
     </div>
+
 
   );
 };
