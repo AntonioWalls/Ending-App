b8596225abe62d6cbf8f8d0a8e947308333d149f
AntonioWalls <psfa.fabian@gmail.com>
2024-05-06 08:46:33 -0700

    WOW UNA API

---
+++ API-ending/API-ending/Entidades/usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace API_ending.Entidades
+{
+    public class usuario
+    {
+    public string id { get; set; }
+
+    public string Nombre {  get; set; }
+    public string Primer_apellido {  get; set; }
+    public string Primer_apellido { get; set; }
+
+    public string Primer_apellido { get; set; }
+
+    public string Primer_apellido { get; set; }
+    public string Primer_apellido { get; set; }
+    public string Primer_apellido { get; set; }
+    public string Primer_apellido {  get; set; }
+
+    }
+
+
+
+}
